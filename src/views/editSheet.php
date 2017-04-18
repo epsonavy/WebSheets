a6527a7ba92ab339f6a3a459cc00d3fbb400a962
Pei Lian Liu <epsonavy@yahoo.com>
2017-04-18 16:14:47 -0700

    added edit sheet page

---
+++ src/views/editSheet.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace nighthawk\hw4\views;
+
+require_once('view.php');
+require_once('elements/h1.php');
+require_once('elements/link.php');
+require_once('helpers/url.php');
+require_once('helpers/sheetEditable.php');
+require_once('layouts/beginLayout.php');
+require_once('layouts/endLayout.php');
+
+class EditSheetView extends View {
+	public function render($data) {
+		if ($data) {
+			if ($data[1]) {
+				print_r($data[1]);
+			}
+		}
+		//dummy data for testing
+		$data[0] = "dummy title";
+		$data[1] = "[[\"Peter\", \"50\"],[\"Kevin\", \"6\"]]";
+		$data[2] = "Edit";
+		$data[3] = "12345678";
+		$data[4] = "Read";
+		$data[5] = "12345678";
+		$data[6] = "File";
+		$data[7] = "12345678";
+
+		$h1 = new \nighthawk\hw4\elements\H1();
+		$link = new \nighthawk\hw4\elements\Link();
+		$url = new \nighthawk\hw4\helpers\Url();
+		$sheet = new \nighthawk\hw4\helpers\SheetEditable();
+		$beginLayout = new \nighthawk\hw4\layouts\BeginLayout();
+		$endLayout = new \nighthawk\hw4\layouts\EndLayout();
+
+		$titleLink = array("index.php", "Web Sheets");
+
+		echo $beginLayout->render($data);
+		echo $h1->render($link->render($titleLink)." : ".$data[0]);
+		echo $url->render(array($data[2], $data[3]));
+		echo $url->render(array($data[4], $data[5]));
+		echo $url->render(array($data[6], $data[7]));
+		echo $sheet->render($data[1]);
+		echo $endLayout->render($data);
+	}
+}
+
+?>
--- src/views/helpers/url.php
+++ src/views/helpers/url.php
@@ -6,7 +6,15 @@ require_once('helper.php');
 
 class Url extends Helper {
 	public function render($data) {
-        return $data[0].' Url : <input type="text" name="code" value="'.$data[1].'" />';
+        if (strcasecmp($data[0], "edit") == 0) {
+            return 'Edit Url : <input type="text" name="code" value="'.$data[1].'" /><br />';
+        } else if (strcasecmp($data[0], "read") == 0) {
+            return 'Read Url : <input type="text" name="code" value="'.$data[1].'" /><br />';
+        } else if (strcasecmp($data[0], "file") == 0) {
+            return 'File Url : <input type="text" name="code" value="'.$data[1].'" /><br />';
+        } else {
+            return $data[0].' Url : <input type="text" name="code" value="'.$data[1].'" /><br />';
+        }      
 	}
 }
 ?>
