982f142e75a8ad8a391bfc6b4b12a39dccc5b10a
Pei Lian Liu <epsonavy@yahoo.com>
2017-04-18 18:52:05 -0700

    added validateForm

--- src/views/helpers/form.php
+++ src/views/helpers/form.php
@@ -7,7 +7,7 @@ require_once('helper.php');
 class Form extends Helper {
 	public function render($data) {
         ?>
-		<form method="get" action="index.php?c=landing&m=form" onsubmit="return validateForm()">
+		<form method="get" action="index.php?c=editSheet&m=form" name="myForm" onsubmit="return validateForm()">
         <input type="text" name="name" placeholder="New sheet name or code" />
         <input type="submit" value="Go" />
         </form>
@@ -18,6 +18,17 @@ class Form extends Helper {
 
 <script>
 function validateForm() { 
-    return true;
+    var x = document.forms["myForm"]["name"].value;
+    if (x == "") {
+        alert("Name or hash code must be filled out");
+        return false;
+    }
+
+    // Check whitespace
+    x = x.replace(/^\s+/, '').replace(/\s+$/, '');
+    if ( x == '') {
+        alert("Whitespace is not allow, please enter correct name!");
+        return false;
+    }
 }
 </script>
