6ab7487dade9d2a32b05b4d024dc089578fe7583
Pei Lian Liu <epsonavy@yahoo.com>
2017-04-19 01:14:57 -0700

    updated readsheetController

--- src/controllers/readSheet.php
+++ src/controllers/readSheet.php
@@ -23,11 +23,27 @@ class ReadSheetController extends Controller {
 
         $model = new \nighthawk\hw4\models\ReadSheetModel();
         $view = new \nighthawk\hw4\views\ReadSheetView();
-        $array = Array();
+        $newRes = Array();
+        $md5_f = "";
 
         $model->initConnection();
         
-        $view->render($array);
+        if (isset($_REQUEST['code'])) {
+            echo "Analyzing hashcode : ".$_REQUEST['code']." ...";
+            $dataFromCode = $model->getDataByCode($_REQUEST['code']);
+            $id = $dataFromCode[0];
+            $dataFromId = $model->getDataById($id);
+            $name = $dataFromId[0];
+
+            $md5_f = substr(md5($name.'file'), 0, 7);
+
+            array_push($newRes, $name);
+            array_push($newRes, $dataFromId[1]);
+            array_push($newRes, "File");
+            array_push($newRes, $md5_f);
+        } 
+
+        $view->render($newRes);
     }
 
 }
